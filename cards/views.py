d1b91df4e489accf546f07e456d57d89243bc7e7
Marisa Lenci <Marisa@Marisas-Mac-Mini.local>
2020-06-26 13:05:07 -0700

    added game page

--- cards/views.py
+++ cards/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.contrib.auth.models import User
 from .models import Card, Hand, Game
 
 def home(request):
     return render(request, 'home.html')
 
-def game(request):
+def create_game(request):
     if request.method == 'GET':
         player = request.user
         game = Game.objects.create()
@@ -22,5 +23,25 @@ def game(request):
             player_id = request.POST.get(player)
             game.players.add(player_id)
         game.save()
-        return redirect('home')
-    return render(request, 'game.html', context)
+        return redirect(reverse('play-game', 
+            kwargs={'game_id':game.id}))
+    return render(request, 'create_game.html', context)
+
+def play_game(request, game_id):
+    game = Game.objects.get(id=game_id)
+    players = game.players.all()
+    card_groups = game.deal()
+    for i in range(4):
+        hand, created = Hand.objects.get_or_create(
+            user=players[i])
+        hand.cards.clear()
+        for card in card_groups[i]:
+            hand.cards.add(card)
+        hand.save()
+    context = {
+        'game': game,
+        'hand': Hand.objects.get(
+            user=request.user),
+        'players': players,
+    }
+    return render(request, 'play_game.html', context)
